4a38fea9324ebd6bd4a3fb9e2650995fedafa0f6
setsuna <c0b22140fd@edu.teu.ac.jp>
2023-04-25 15:48:35 +0900

    練習問題6

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -78,7 +78,9 @@ def main():
             vx *= -1
         if not tate:
             vy *= -1
-        screen.blit(bb_img, bb_rect)
+        screen.blit(bb_img, bb_rect)  #　練習３
+        if kk_rect.colliderect(bb_rect):
+            return
 
         pg.display.update()
         clock.tick(1000)
